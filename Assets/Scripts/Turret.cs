5dcdbcce6d457053f330e262b56bb533bf058209
KawaiiSelbst <mrsmailbot.lg@gmail.com>
2022-05-09 17:20:11 +0300

    Add project files

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D fireBall;
+
+    private List<Rigidbody2D> fireballInstances;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SomeCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private Rigidbody2D ShootFireball()
+    {
+        Rigidbody2D fireBallInstance = Instantiate(fireBall, transform.position, transform.rotation);
+        fireBallInstance.velocity = transform.right * -70;
+        return fireBallInstance;
+    }
+    
+    IEnumerator SomeCoroutine()
+    {
+        //Declare a yield instruction.
+        WaitForSeconds wait = new WaitForSeconds(1);
+
+        while (true)
+        {
+            ShootFireball();
+            yield return wait; //Pause the loop for 3 seconds.
+        }
+    }
+}
