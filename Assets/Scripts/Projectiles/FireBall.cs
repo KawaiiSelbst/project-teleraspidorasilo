8e2fb1fb6b0245536a08998703b88ac432afc0cd
Kawaii_Selbst <mrsmailbot.lg@gmail.com>
2022-05-29 06:15:15 +0300

    #11 Move fireball prefab to `Projectiles` folder Add `OnCollisionEnter2D` method that instantiate explosion prefab and run `SelfDeletion`

---
+++ Assets/Scripts/Projectiles/FireBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class FireBall : Projectile
+{
+    [SerializeField] private float _lifeTime = 10f;
+    [SerializeField] private GameObject _explotionPrefab;
+    private bool _isAlive = true;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        SelfDeletion();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_isAlive)
+        {
+            _isAlive = false;
+            SelfDeletion();
+            Instantiate(_explotionPrefab, transform.position, transform.rotation);
+        }
+
+    }
+}
