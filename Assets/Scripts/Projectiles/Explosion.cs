f591bb54dcf98468b617c35974c8a3875b3dcdd1
Kawaii_Selbst <mrsmailbot.lg@gmail.com>
2022-05-29 06:12:49 +0300

    #11 Add explosion `prefab` and `class`

---
+++ Assets/Scripts/Projectiles/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime = .25f;
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        GameObject.Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
